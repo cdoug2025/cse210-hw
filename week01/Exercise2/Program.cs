47b0ff0ceb2092cc5b4e02828a0459f860a626ac
cdoug2025 <chris5617@hotmail.com>
2025-09-06 02:45:57 -0400

    Completed Exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        //get input from user
+        Console.Write("What is your grade percentage? ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+        string gradeLetter;
+
+        //determine letter
+        if (gradePercentage >= 90)
+        {
+            gradeLetter = "n A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            gradeLetter = " B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            gradeLetter = " C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            gradeLetter = " D";
+        }
+        else
+        {
+            gradeLetter = "n F";
+        }
+
+        //determine sign
+        string gradeSign = "";
+
+        if (!(gradeLetter == "n F"))
+        {
+            if ((gradePercentage % 10) >= 7 && !(gradeLetter == "n A"))
+            {
+                gradeSign = "+";
+            }
+            else if ((gradePercentage % 10) < 3)
+            {
+                gradeSign = "-";
+            }
+        }
+
+        //print
+            Console.WriteLine($"\nA grade percentage of {gradePercentage}% would give a{gradeLetter}{gradeSign}");
     }
 }
