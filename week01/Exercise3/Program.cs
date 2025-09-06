8bba31cb4dba5204b76b6b5f8bca3e1d3915fbbf
cdoug2025 <chris5617@hotmail.com>
2025-09-06 03:19:35 -0400

    Completed Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //game loop
+        string playAgain;
+        do
+        {
+            //generate number
+            Console.Write("I'm thinking of a magic number between one and... ");
+            int maxRange = int.Parse(Console.ReadLine());
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, maxRange + 1);
+
+            //loop
+            int guessNumber;
+            do
+            {
+                //guess number
+                Console.Write("\nWhat is your guess? ");
+                guessNumber = int.Parse(Console.ReadLine());
+
+                //higher
+                if (magicNumber > guessNumber)
+                {
+                    Console.WriteLine($"The magic number is higher than {guessNumber}");
+                }
+
+                //lower
+                else if (magicNumber < guessNumber)
+                {
+                    Console.WriteLine($"The magic number is lower than {guessNumber}");
+                }
+
+                //equals
+                else if (magicNumber == guessNumber)
+                {
+                    Console.WriteLine($"You guessed it!\nThe magic number was {magicNumber}");
+                }
+
+            } while (guessNumber != magicNumber);
+
+            //play again
+            Console.Write("\nWould you like to play again (y/n)? ");
+            playAgain = Console.ReadLine();
+        } while (playAgain == "y");
     }
 }
