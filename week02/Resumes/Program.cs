f4123ebf2d9dd5bc9d6266a470753a445819525c
cdoug2025 <chris5617@hotmail.com>
2025-09-10 07:04:08 -0400

    Added customization

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,26 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        //job1
-        Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
-
-        //job2
-        Job job2 = new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
-
-        //resume
+        //add resume
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
-        myResume._jobs.Add(job1);
-        myResume._jobs.Add(job2);
 
+        //name
+        Console.Write("Please enter your name: ");
+        myResume._name = Console.ReadLine();
+
+        //add job
+        string addJob;
+        do
+        {
+            Job job = new Job();
+
+            //company
+            Console.Write("Please enter the company you worked for: ");
+            job._company = Console.ReadLine();
+
+            //position
+            Console.Write("Please enter the position you worked: ");
+            job._jobTitle = Console.ReadLine();
+
+            //start year
+            Console.Write("Please enter the year you started working this job: ");
+            job._startYear = int.Parse(Console.ReadLine());
+            
+            //end year
+            Console.Write("Please enter the year you finished working this job: ");
+            job._endYear = int.Parse(Console.ReadLine());
+
+            //add to list
+            myResume._jobs.Add(job);
+
+            //continue loop if user types yes or y
+            Console.Write("Would you like to add another job? (y/n): ");
+            addJob = Console.ReadLine();
+            if (addJob == "yes")
+            {
+                addJob = "y";
+            }
+        } while (addJob == "y");
+
+        //display resume
+        Console.WriteLine();
         myResume.Display();
     }
 }
