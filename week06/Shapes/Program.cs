0f763ed5a50053649cc63a6b80701368c843883b
cdoug2025 <chris5617@hotmail.com>
2025-10-08 20:20:25 -0400

    Completed Week 6 Shapes Activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Runtime.Intrinsics.Arm;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new([
+            new Square("red", 4),
+            new Square("yellow", 1.05),
+            new Rectangle("pink", 7, 3),
+            new Rectangle("green", 8.45, 0.36),
+            new Circle("blue", 2),
+            new Circle("red", 6.08),
+        ]);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The color of this {shape} is {shape.GetColor()} and the area is {shape.GetArea()}");
+        }
     }
 }
